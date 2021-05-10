975cce2a3da66d18d3e6daf525da041b0272e526
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-05-10 23:23:39 +0530

    Init Commit

---
+++ app/src/main/java/com/example/referbussinessapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.referbussinessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
