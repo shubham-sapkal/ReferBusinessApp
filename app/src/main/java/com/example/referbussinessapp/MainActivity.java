d43fe09ccab4a66870415fd3906a80e672d352a4
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-05-10 23:42:54 +0530

    Added function to call register activity

--- app/src/main/java/com/example/referbussinessapp/MainActivity.java
+++ app/src/main/java/com/example/referbussinessapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showRegisterActivity(View view){
+
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+
     }
 }
