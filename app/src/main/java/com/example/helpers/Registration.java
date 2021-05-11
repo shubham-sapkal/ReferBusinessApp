4acc6fa23f9bd4a693d7e46c373030f791ffcb5d
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-05-11 23:55:07 +0530

    Added Helper Registration file

---
+++ app/src/main/java/com/example/helpers/Registration.java
@@ -0,0 +1,38 @@
+package com.example.helpers;
+
+public class Registration {
+
+    String name,username,email,password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
