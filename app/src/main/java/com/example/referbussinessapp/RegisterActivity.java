821a4ac5af25d192f5e71872e444464d5233b78d
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-05-11 23:54:13 +0530

    modified code

--- app/src/main/java/com/example/referbussinessapp/RegisterActivity.java
+++ app/src/main/java/com/example/referbussinessapp/RegisterActivity.java
@@ -3,12 +3,29 @@ package com.example.referbussinessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText nameEditText;
+    TextInputEditText usernameEditText;
+    TextInputEditText emailEditText;
+    TextInputEditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void doRegister(View view){
+
+        nameEditText = (TextInputEditText) findViewById(R.id.nameEditText);
+        usernameEditText = (TextInputEditText) findViewById(R.id.usernameEditText);
+        emailEditText = (TextInputEditText) findViewById(R.id.emailEditText);
+        passwordEditText = (TextInputEditText) findViewById(R.id.passwordEditText);
+
     }
 }
